f8cce4926084240eae7814dace75e7ec7a98e44b
Candido Souza <candido_souzza@hotmail.com>
2015-09-01 19:53:51 -0300

    Mostrando resultados da busca

--- js/main.js
+++ js/main.js
@@ -24,4 +24,74 @@ $(function() {
             },400, function() {});
         }
     });
+
+	$('#search-form').submit(function (e) {
+		e.preventDefault();
+	});
 });
+
+function search(){
+	// clear results
+	$('#results').html('');
+	$('#buttons').html('');
+	
+	// get form Input
+	q = $('#query').val();
+	
+	// run GET request on API
+	$.get(
+		"https://www.googleapis.com/youtube/v3/search",{
+			part: 'snippet, id',
+			q: q,
+			type:'video',
+			key: 'AIzaSyCvk3NNMQASZgFkCNxIp9jH-l8O0PXhDUo'
+		},function(data){
+				var nextPageToken = data.nextPageToken;
+				var prevPageToken = data.prevPageToken;
+				
+				// log data
+				console.log(data);
+
+				$.each(data.items, function(i, item){
+					// get output
+					var output = getOutput(item);
+
+					// display results
+					$('#results').append(output);
+				});
+
+				//var buttons = getButtons(prevPageToken, nextPageToken);
+                //
+				//// display buttons
+				//$('#buttons').append(buttons);
+		});
+}
+
+//build output
+function getOutput(item){
+	var videoId = item.id.videoId;
+	var title = item.snippet.title;
+	var description = item.snippet.description;
+	var thumb = item.snippet.thumbnails.high.url;
+	var channelTitle = item.snippet.channelTitle;
+	var videoDate = item.snippet.publishedAt;
+
+	// Build Output String
+	var output = '<div class="col-md-3">' +
+		'<div class="panel">' +
+			'<article>' +
+				'<div class="thumbnail">' +
+				'<img src="'+thumb+'">' +
+				'<header>' +
+					'<h1><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/'+videoId+'">'+title+'</a></h1>' +
+				'</header>' +
+				'<small>By <span class="cTitle">'+channelTitle+'</span> on '+videoDate+'</small>' +
+				'<p>'+description.substr(0, 50) +'...</p>' +
+			'</div>' +
+			'</article>' +
+		'</div>' +
+		'</div>' +
+		'';
+
+	return output;
+}
